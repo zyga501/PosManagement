06eb8f5ff0c75e6d3483fd2f88a43fc23a9bf726
zyga501 <x02540431>
2016-01-11 20:08:44 +0800

    优化2

--- src/com/posmanagement/utils/StringUtils.java
+++ src/com/posmanagement/utils/StringUtils.java
@@ -1,5 +1,8 @@
 package com.posmanagement.utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StringUtils {
     public static String convertNullableString(Object object) {
         if (object == null) {
@@ -14,5 +17,16 @@ public class StringUtils {
             return true;
 
         return false;
+    }
+
+
+    public boolean isAdmin(String UID){
+        ArrayList<HashMap<String, Object>> dbRet = null;
+        try {
+            dbRet = PosDbManager.executeSql("select 1 from userinfo where   rid ='e664d6f3-85f8-4bd6-bcb8-c4e053732b29' and  uid='"+UID+"'");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return (dbRet.size() > 0);
     }
 }

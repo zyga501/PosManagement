2efc5afacaafcf53f39b0ac130c8a30a17d33e44
unknown <VonChenPlus@gmail.com>
2016-01-12 22:29:51 +0800

    perfect bill ui

--- src/com/posmanagement/webui/BillUI.java
+++ src/com/posmanagement/webui/BillUI.java
@@ -20,21 +20,19 @@ public class BillUI extends WebUI {
                     .addAttribute("role", "row")
                     .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("BANKNAME")))
                     .addElement("td", StringUtils.formatCardNO(StringUtils.convertNullableString(dbRet.get(index).get("CARDNO"))))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("BILLDATE")))
+                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("BILLDATE")).substring(0, 7))
                     .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("LASTREPAYMENTDATE")))
                     .addElement(new UIContainer("td")
                             .addElement(new UIContainer("label",StringUtils.convertNullableString(dbRet.get(index).get("BILLAMOUNT")).equals("")?"0":dbRet.get(index).get("BILLAMOUNT").toString())
                             .addAttribute("name","billamount")
                             .addAttribute("datav",StringUtils.convertNullableString(dbRet.get(index).get("UUID")))
-                            .addAttribute("style","display:inline-block;width:30")))
+                            .addAttribute("style","display:inline-block;")))
                     .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("CANUSEAMOUNT")))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("BILLHADPAY")))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("BILLNOPAY")))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("LASTPAY")))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("LASTPAYDATETM")))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("LASTTELLER")))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("SALESMAN")))
-                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("EXPIRED")))
+                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("REPAYAMOUNT")))
+                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("REMAINAMOUNT")))
+                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("SALEMAN")))
+                    .addElement("td", StringUtils.convertNullableString(dbRet.get(index).get("REMAINAMOUNT")).compareTo("0") == 0 ?
+                            getText("bill.billfinished") : getText("bill.billunfinished"))
                     .addElement(new UIContainer("td").addElement(new UIContainer("input")
                         .addAttribute("class", dbRet.get(index).get("STATUS").equals("enable")?"btn btn-success radius":"btn btn-danger radius")
                         .addAttribute("type","button")
@@ -47,11 +45,35 @@ public class BillUI extends WebUI {
     }
 
     private ArrayList<HashMap<String, Object>> fetchBillList() throws Exception {
-        if (null==userID_ || userID_.equals(""))
-            return PosDbManager.executeSql("select billtb.*,banktb.name bankname from billtb inner join banktb on banktb.uuid=billtb.bankuuid  order by billtb.BILLDATE desc");
-        else
-            return PosDbManager.executeSql("select billtb.*,banktb.name bankname from billtb inner join banktb on " +
-                    "banktb.uuid=billtb.bankuuid inner join  cardtb on cardtb.cardno=billtb.cardno where  cardtb.salesmanuuid='"+userID_+"' order by billtb.BILLDATE desc");
+        String whereSql = "";
+        if (userID_ != null && !userID_.isEmpty()) {
+            whereSql = "where billtb.salesmanuuid='\"+userID_+\"'";
+        }
+
+        return PosDbManager.executeSql("SELECT\n" +
+                "billtb.uuid,\n" +
+                "billtb.cardno,\n" +
+                "billtb.billamount,\n" +
+                "billtb.`status`,\n" +
+                "billtb.billdate,\n" +
+                "Sum(CASE WHEN repaytb.tradestatus='enable' then repaytb.trademoney else 0 END) AS repayamount,\n" +
+                "SUM(CASE WHEN repaytb.tradestatus='enable' then repaytb.trademoney else 0 END) + billtb.canuseamount AS canuseamount,\n" +
+                "billamount - SUM(CASE WHEN repaytb.tradestatus='enable' then repaytb.trademoney else 0 END) AS remainamount,\n" +
+                "banktb.`name` AS bankname,\n" +
+                "billtb.lastrepaymentdate,\n" +
+                "userinfo.unick AS saleman\n" +
+                "FROM\n" +
+                "billtb\n" +
+                "INNER JOIN repaytb ON CONVERT(repaytb.repayyear, SIGNED)= CONVERT(SUBSTR(billtb.lastrepaymentdate,1,4), SIGNED) AND convert(repaytb.repaymonth, SIGNED)= convert(SUBSTR(billtb.lastrepaymentdate,6,2), SIGNED)\n" +
+                "INNER JOIN banktb ON banktb.uuid = billtb.bankuuid\n" +
+                "LEFT JOIN userinfo ON userinfo.uid = billtb.salesmanuuid\n" +
+                whereSql +
+                "GROUP BY\n" +
+                "billtb.cardno,\n" +
+                "SUBSTR(billtb.lastrepaymentdate,1,4),\n" +
+                "SUBSTR(billtb.lastrepaymentdate,6,2)\n" +
+                "ORDER BY\n" +
+                "billtb.billdate DESC");
     }
 
     private String userID_; // TODO for role
